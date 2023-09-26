57b23febc12d37fe621d58e8655cf23489b6a03b
xhc12138 <108609389+xhc12138@users.noreply.github.com>
2023-09-26 02:18:29 -0500

    Add files via upload

---
+++ xhc_resume/show_image.js
@@ -0,0 +1,6 @@
+
+      const showImageButton = document.getElementById("show-image-button");
+      const myImage = document.getElementById("my-image");
+      showImageButton.addEventListener("click", () => {
+        myImage.style.display = "block";
+      });
